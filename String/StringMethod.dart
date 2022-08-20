b9046d8cb98f977116f87644e895455484c9292f
rajeshsaharan <rajeshsharan1607@gmail.com>
2022-08-20 16:26:27 +0530

    dart

--- String/StringMethod.dart
+++ String/StringMethod.dart
@@ -1,3 +1,8 @@
-void main(){
+void main() {
+  //top string method in dart
+  String name = "rajesh";
+  // basically in dart everything is an object that have property and method;
+  // .toUppercase()
+  //. contains() to check something
   
 }
