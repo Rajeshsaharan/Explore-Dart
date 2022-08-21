7bfe52cc0905d0f67ad8476272b5d3b6d1f1de2a
rajeshsaharan <rajeshsharan1607@gmail.com>
2022-08-21 13:06:48 +0530

    dart

--- null-safety/null.dart
+++ null-safety/null.dart
@@ -40,12 +40,41 @@ void main() {
   bool flag = true;
   if (flag) {
     num3 = 20;
+  } else {
+    num3 = 0;
   }
+
   // now if we assign
   num4 +
       num3; //that will give error because we are assigning nullable value to non-nullable value
   //so we can do that in better way
 
-  num4 + num3!; //now we are forcing to because we know num3 will not be null at that time
+  num4 +
+      num3!; //now we are forcing to because we know num3 will not be null at that time
 
+//null aware
+
+  if (num3 == null) {
+    num3 = 0;
+  } else {
+    num3 = 20;
+  }
+  // we can do that in ternary operator
+
+  num3 = num3 == null ? 0 : 20;
+
+  //but we can do that another way
+  //if num3 is null assign value 0 to it;
+
+  num3 = num3 ?? 0;
+
+  //null aware operator
+
+  var list = ["item1", "item2", null];
+  for (var item in list) {
+    print(item!
+        .length); // now we are forcing to perform action because we know item cant be null --> not good practice
+    //either way can it in better way
+    print(list?.length); //here we are checking if there are some null value then just return null
+  }
 }
